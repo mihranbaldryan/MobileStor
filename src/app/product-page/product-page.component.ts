5088f4c78a12e469d55faa5b69885e045a50d1d8
Zaven Sargsyan <zvsargsyan@gmail.com>
2018-06-29 16:33:50 +0400

    Zaven

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit  } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-product-page',
@@ -9,13 +10,15 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ProductPageComponent implements OnInit {
     card;
+
   public constructor(private route: ActivatedRoute) {
     this.route.queryParams.subscribe(params => {
       this.card=params;
     })
    }
-
   ngOnInit() {
+      // $("#table > tbody > tr").has('td:empty').hide();
+      // $("'.thead-dark' ").parent().hide()
   }
 
 }
